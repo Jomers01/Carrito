b17b8b3ff5d7c553f087f152db938e6cebb3bf02
Jomers01 <wearponbest@gmail.com>
2021-09-06 17:14:14 -0500

    data

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,11 @@
 //variables
+const url = 'https://jomers01.github.io/Carrito/scripts/data.json'
 const contP = document.getElementById('container-principal');
 const contC = document.getElementById('cafeteria');
 const cart = document.getElementById('carrito');
 const btnCart = document.getElementById('btnCart');
 const btnCafe = document.getElementById('cafe');
 const btnPapel = document.getElementById('papel');
-
-//Importamos los productos desde la data
-import productos from './data.js'
 
 //Cuando apenas se ejecute la pagina llamamos a que se muestre los articulos de la cafetería en pantalla
 document.addEventListener('DOMContentLoaded', () => {
@@ -19,29 +17,35 @@ btnCafe.addEventListener('click', mostarItemsCafeteria);
 btnPapel.addEventListener('click', mostarItemsPapeleria);
 
 //Funcion para mostrar el contenido solo de la cafeteria 
-function mostarItemsCafeteria() {
+async function mostarItemsCafeteria() {
+    const res = await fetch(url);
+    const productos = await res.json();
     //Limpiamos el contenedor
     contC.innerHTML = '';
 
     //Mostramos todos los productos del array
-    productos.cafeteria.forEach(items => {
+    productos.productos.forEach(items => {
         //Desectructuramos
         const { nombre, precio, img, id } = items;
+        const itemss = document.createElement('div')
         //Imprimimos
-        contC.innerHTML +=
+        itemss.innerHTML +=
             `<div class="card" style="width: 12rem; margin-right: 20px; margin-top: 20px;">
             <img src="${img}" class="card-img-top" alt="item" id="img">
             <div class="card-body">
                 <h5 class="card-title">${nombre}</h5>
                 <p>$ ${precio} COP</p>
-            <a class="btn btn-primary" onclick="agregar(id)" id="${id}">Agregar al carrito</a>
+            <a class="btn btn-primary" onclick="agregarPeli(id)" id="${id}">Agregar al carrito</a>
             </div>
         </div>`
+        contC.appendChild(itemss)
     });
 }
 
 //Funcion para mostrar el contenido solo de la papaleria 
-function mostarItemsPapeleria() {
+async function mostarItemsPapeleria() {
+    const res = await fetch(url);
+    const productos = await res.json();
     //Limpiamos el contenedor
     contC.innerHTML = '';
 
@@ -62,7 +66,7 @@ function mostarItemsPapeleria() {
     });
 }
 
-function agregar(id) {
+function agregarPeli(id) {
     console.log(id);
 }
 
